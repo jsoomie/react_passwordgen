3e7889e38911b8302e24d9cac0c597167f2b08ab
Jonathan Soukaseume <sksmejn@gmail.com>
2021-06-14 18:37:58 -0700

    Created template of missing 404 page.

---
+++ src/pages/Missing.js
@@ -0,0 +1,11 @@
+import { Container, Typography } from "@material-ui/core";
+
+export const Missing = () => {
+  return (
+    <Container>
+      <Typography>Missing</Typography>
+    </Container>
+  );
+};
+
+export default Missing;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,7 @@
+// Pages index
+
 //@export   Home page
 export { Home } from "./Home";
+
+//@export Missing 404 page
+export { Missing } from "./Missing";
